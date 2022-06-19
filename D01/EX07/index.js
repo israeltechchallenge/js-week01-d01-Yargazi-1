045b998b42ff874733cf9d81ccfc82cec3e3d1b3
Yargazi <94797678+Yargazi@users.noreply.github.com>
2022-06-19 23:25:38 +0300

    first commit

--- D01/EX07/index.js
+++ D01/EX07/index.js
@@ -1,7 +1,29 @@
-function getRandomInt(max) {
-  //your code
+let number; 
+let comparer;
+const outputResult = document.getElementById("output");
+const сalculateBtn = document.getElementById('сalculate');
+
+
+function moduloEx6(number, comparer) {
+  if (number === comparer) {
+    console.log(`The number ${number} is equal to ${comparer}`)
+    outputResult.innerHTML =`The number ${number} is equal to ${comparer}`;
+  } else if (number < comparer) {
+    outputResult.innerHTML =`The number ${number} is less than ${comparer}`;
+  } else {
+    outputResult.innerHTML =`The number ${number} is bigger than ${comparer}`;
+  }
+  (number > comparer && number % comparer > 0) ? outputResult.innerHTML =`The number ${number} is bigger than ${comparer}. But the modulo of ${number} % ${comparer} is ${number % comparer}` : false;
 }
 
-function moduloEx7() {
-  //your code
-}
+const getRandomNum = (number, comparer) => {
+  number = Math.floor(Math.random() * 99);
+  comparer = Math.floor(Math.random() * 99);
+  console.log(`this is number ${number}`);
+  console.log(`this is comparer ${comparer}`);
+  moduloEx6(number, comparer);
+  return number, comparer;
+} 
+getRandomNum()
+
+сalculateBtn.addEventListener("click", getRandomNum); 
